6586c7c7a098bd088b55b4ca1f03ece41bfe20f1
faizan6557 <aitisamahmed24@gmail.com>
2024-08-25 11:49:03 +0500

    update the code

--- miniagi.py
+++ miniagi.py
@@ -3,13 +3,13 @@ import subprocess
 
 
   
-if __name__ == "__main__":clear
+if __name__ == "__main__":
 
     user_input = input("Enter a string to check if it's a palindrome: ")
-    check_palindrome(user_input)
+    # check_palindrome(user_input)
     # Example usage
     # Example usage
     # Example usage
 
     # Run Git operations
-    run_git_commands()
+    # run_git_commands()
